d963a769341cb058e5131debdfc3e5afb5b25b62
KSmith8888 <83607802+KSmith8888@users.noreply.github.com>
2021-09-07 16:21:40 -0700

    Update script3.js
    JS to clear contact form after submission.

--- script3.js
+++ script3.js
@@ -1,15 +1,5 @@
-let censusData = document.getElementById('censusData');
-
-function getCensusData() {
-    let Http = new XMLHttpRequest;
-    let url = 'https://api.census.gov/data/timeseries/eits/mwts?get=cell_value,data_type_code,time_slot_id,category_code,seasonally_adj&time=2020-12';
-    Http.open("GET", url);
-    Http.send()
-    Http.onreadystatechange = function() {
-        if(this.readyState==4 && this.status==200) {
-            censusData.textContent = JSON.parse(Http.response);
-        }
+window.onbeforeunload = () => {
+    for(const form of document.getElementsByTagName('form')) {
+      form.reset();
     }
-}
-
-window.onload = getCensusData;
+  }
